39867fc14537d2e7bf18d05a6737096788a2a460
Ira Filonova <74858792+wonder-filka@users.noreply.github.com>
2022-10-25 15:36:14 +0300

    added styles

--- src/style.js
+++ src/style.js
@@ -1,3 +1,14 @@
+$(window).bind("resize", resizeWindow);
+function resizeWindow(e) {
+  var newWindowWidth = $(window).width();
+
+  // Если ширина меньше 600 px, используется таблица стилей для мобильного
+  if (newWindowWidth < 881) {
+  } else if (newWindowWidth > 900) {
+    // Если ширина больше 600 px, используется таблица стилей для десктопа
+  }
+}
+
 $(document).ready(function () {
   $(".btn").click(function () {
     $(this).addClass("btnAterClick");
@@ -5,5 +16,9 @@ $(document).ready(function () {
 
   $(".copy").click(function () {
     $(this).text("Copied!");
+  });
+
+  $(".menuMobile").click(function () {
+    $(".listMobile").toggle();
   });
 });
