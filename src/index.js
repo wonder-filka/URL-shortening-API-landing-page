25a770de6ba69008566673d3ce8b31dc5688bec8
Ira Filonova <74858792+wonder-filka@users.noreply.github.com>
2022-10-26 22:34:23 +0300

    added js part

---
+++ src/index.js
@@ -0,0 +1,54 @@
+let form = document.querySelector("#searchForm");
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let inputUser = document.querySelector("#inputSearch");
+  getData(inputUser.value);
+}
+
+function getData(link) {
+  let apiUrl = `https://api.shrtco.de/v2/shorten?url=${link}`;
+  axios.get(apiUrl).then(showResult);
+}
+
+function showResult(response) {
+  let result = document.querySelector("#result");
+  let resultWrap = '<div class="resultWrap">';
+  let originLink = response.data.result.original_link;
+  let shortLinks = [
+    response.data.result.short_link,
+    response.data.result.short_link2,
+    response.data.result.short_link3,
+  ];
+
+  shortLinks.forEach(function (elem) {
+    resultWrap =
+      resultWrap +
+      `<div class="resultItem">
+                    <div class="link">${originLink}</div>
+                    <div class="newLink"> ${elem}</div>
+                    <button type="button" class="btn copy">Copy</button>
+                </div>`;
+  });
+
+  resultWrap = resultWrap + `</div>`;
+  result.innerHTML = resultWrap;
+
+  let resultItem = document.querySelectorAll(".resultItem");
+  resultItem.forEach(function (item) {
+    let itemCopy = item.querySelector(".copy");
+
+    itemCopy.addEventListener("click", copied);
+    function copied(event) {
+      event.preventDefault();
+      let newLink = item.querySelector(".newLink");
+      let newLinkCopy = newLink.textContent;
+      navigator.clipboard.writeText(newLinkCopy);
+      itemCopy.classList.add("btnAterClick");
+      console.log(newLinkCopy);
+      itemCopy.innerText = "Copied!";
+    }
+  });
+}
--- src/style.js
+++ src/style.js
@@ -1,21 +1,6 @@
-$(window).bind("resize", resizeWindow);
-function resizeWindow(e) {
-  var newWindowWidth = $(window).width();
-
-  // Если ширина меньше 600 px, используется таблица стилей для мобильного
-  if (newWindowWidth < 881) {
-  } else if (newWindowWidth > 900) {
-    // Если ширина больше 600 px, используется таблица стилей для десктопа
-  }
-}
-
 $(document).ready(function () {
   $(".btn").click(function () {
     $(this).addClass("btnAterClick");
-  });
-
-  $(".copy").click(function () {
-    $(this).text("Copied!");
   });
 
   $(".menuMobile").click(function () {
